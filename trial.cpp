de03d9b925b88be6b5beced2412360a23ab4c44f
moyojs <36846692+moyojs@users.noreply.github.com>
2018-02-26 16:30:47 +0100

    Create trial.cpp
    This was just to remind myself about c++

---
+++ trial.cpp
@@ -0,0 +1,13 @@
+/*I haven't written a program in a long time.
+  It's been more than a year, I think, since I wrote code
+  (Not sure if that's proper english)
+  Well, I'm about to start again. Wish me luck */
+	
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is something";
+}
